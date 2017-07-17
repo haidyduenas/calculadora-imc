7c28956538c1cd8fd6e7ddec47b3c72538df957a
paola <bernodiana@gmail.com>
2017-07-17 12:03:29 -0500

    corregir

--- archivo.js
+++ archivo.js
@@ -1,12 +1,10 @@
 var peso = parseInt(prompt("Ingrese su peso en kilogramos"));
 var estatura = parseInt(prompt("Ingrese su estatura"));
-var resultado = (peso(estatura*estatura)*10000).toFixed(2);
-alert("Tu (IMC es " + resultado);
-/*function calculadora(n,m){
+
+function calculadora(n,m){
 	var resultado = n/(m*m);
 	var respuesta= resultado*10000;
 	var ultimo = respuesta.toFixed(2);
-	document.write("Su indice de IMC es " + ultimo) ;
+	alert("Su indice de IMC es " + ultimo) ;
 }
 calculadora(peso,estatura);
-*/
